754c3ac71b86b30d6c88b75fd853d941787dec47
FennatenHaaf <61841371+FennatenHaaf@users.noreply.github.com>
2021-02-01 15:46:09 +0100

    change import in main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from knab_dataprocessor import *
+import knab_dataprocessor as knabData
 import utils
 
 # Todo Aparte main gecrëerd zodat knabprocessor ook apart te runnen is.
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     indirec = "./data"
     outdirec = "./output"
     interdir = "./interdata"
-    datatest = data_linking(indirec,interdir,outdirec)
+    datatest = knab_dataprocessor.data_linking(indirec,interdir,outdirec)
 
     cross_sec = datatest.create_base_cross_section(date_string="2020-12",
                                                    subsample=True,
