35204ac02153d881a417f00dd3d57f7f81e38085
ivankalia <77011392+ivankalia@users.noreply.github.com>
2021-02-02 21:22:17 +0100

    Changes to linking time sets, changes to importing and converting

--- declarationsFile.py
+++ declarationsFile.py
@@ -58,10 +58,60 @@ def getPatColToParseCross(subset="total"):
         print("error getting list")
 
 
+
+
+# Todo test
+def getConvertDict():
+    datatypeGeneralActivity = {
+        'dateeow': "datetime64",
+        'yearweek': "string",
+        'portfolioid': "category",
+        'pakketcategorie': "category"}
+
+    datatypeActivity = {
+        'overstapserviceyn': "uint8",
+        'betaalalertsyn': "uint8",
+        'aantalbetaalalertsubscr': "uint16",
+        'aantalbetaalalertsontv': "uint16",
+        'roodstandyn': "uint8",
+        'saldoregulatieyn': "uint8",
+        'appyn': "uint8",
+        'aantalloginsapp': "uint16",
+        'aantalloginsweb': "uint16",
+        'activitystatus': "category"}
+
+    datatypeTrans = {
+        'betalenyn': "uint8",
+        'saldobetalen': "int32",
+        'aantalbetaaltransacties': "uint16",
+        'aantalatmtransacties': "uint16",
+        'aantalpostransacties': "uint16",
+        'aantalfueltransacties': "uint16",
+        'depositoyn': "uint8",
+        'saldodeposito': "uint32",
+        'flexibelsparenyn': "uint8",
+        'saldoflexibelsparen': "uint32",
+        'kwartaalsparenyn': "uint8",
+        'saldokwartaalsparen': "uint32",
+        'gemaksbeleggenyn': "uint8",
+        'saldogemaksbeleggen': "uint32",
+        'participatieyn': "uint8",
+        'saldoparticipatie': "uint32",
+        'vermogensbeheeryn': "uint8",
+        'saldovermogensbeheer': "uint32",
+        'saldototaal': "int32",
+        'saldolangetermijnsparen': "uint32",
+        'aantaltegenrekeningenlaatsteq': "uint16"}
+
+    return {**datatypeGeneralActivity, **datatypeActivity, **datatypeTrans}
+
+
+
+###----TIME SERIES ---------------------------------------------------------
 def getPatColToParseTS(subset="total"):
     columnsToParse1 = [
         "dateeow",
-        "yearweek",
+        # "yearweek",
         "portfolioid",
         "pakketcategorie"]
 
@@ -113,111 +163,6 @@ def getPatColToParseTS(subset="total"):
                (columnsToParse1 + columnsToParse3)
     else:
         print("error getting list")
-
-# Todo test
-def getPatConvert(subset = "total"):
-    datatypeConvertP1 = {
-        'dateeow': "datetime64",
-        'yearweek': "string",
-        'portfolioid': "category",
-        'pakketcategorie': "category"}
-
-    datatypeConvertP2 = {
-        **datatypeConvertP1 ,
-        'overstapserviceyn': "uint8",
-        # 'betaalalertsyn': "uint8",
-        # 'aantalbetaalalertsubscr': "uint16",
-        # 'aantalbetaalalertsontv': "uint16",
-        'roodstandyn': "uint8",
-        'saldoregulatieyn': "uint8",
-        'appyn': "uint8",
-        'aantalloginsapp': "uint16",
-        'aantalloginsweb': "uint16",
-        'activitystatus': "category"}
-
-    datatypeConvertP3 = {
-        **datatypeConvertP1 ,
-        'betalenyn': "uint8",
-        'saldobetalen': "int32",
-        'aantalbetaaltransacties': "uint16",
-        'aantalatmtransacties': "uint16",
-        'aantalpostransacties': "uint16",
-        'aantalfueltransacties': "uint16",
-        'depositoyn': "uint8",
-        'saldodeposito': "uint32",
-        'flexibelsparenyn': "uint8",
-        'saldoflexibelsparen': "uint32",
-        'kwartaalsparenyn': "uint8",
-        'saldokwartaalsparen': "uint32",
-        'gemaksbeleggenyn': "uint8",
-        'saldogemaksbeleggen': "uint32",
-        'participatieyn': "uint8",
-        'saldoparticipatie': "uint32",
-        'vermogensbeheeryn': "uint8",
-        'saldovermogensbeheer': "uint32",
-        'saldototaal': "int32",
-        'saldolangetermijnsparen': "uint32",
-        'aantaltegenrekeningenlaatsteq': "uint16"}
-
-    datatypeConvertAll = {**datatypeConvertP1, **datatypeConvertP2, **datatypeConvertP3}
-
-    if subset == "total":
-        return {**datatypeConvertP1, **datatypeConvertP2, **datatypeConvertP3}
-    elif subset == "activity":
-        return datatypeConvertP2
-    elif subset == "transaction":
-        return datatypeConvertP3
-    elif subset == "all":
-        return {**datatypeConvertP1, **datatypeConvertP2, **datatypeConvertP3},datatypeConvertP2,\
-               datatypeConvertP3
-    else:
-        print("error getting dictionary")
-
-def getAggFuncDict():
-    aggFuncDictP1 = {
-        'dateeow': min}
-
-    aggFuncDictP2 = {
-        **aggFuncDictP1,
-        'overstapserviceyn': "mean",
-        # 'betaalalertsyn': "mean",
-        # 'aantalbetaalalertsubscr': "uint16",
-        # 'aantalbetaalalertsontv': "uint16",
-        'roodstandyn': "mean",
-        'saldoregulatieyn': "mean",
-        'appyn': "mean",
-        'aantalloginsapp': sum,
-        'aantalloginsweb': sum
-        # 'activitystatus': "category",
-    }
-
-    aggFuncDictP3 = {
-        **aggFuncDictP1,
-        'betalenyn': "uint8",
-        'saldobetalen': "int32",
-        'aantalbetaaltransacties': "uint16",
-        'aantalatmtransacties': "uint16",
-        'aantalpostransacties': "uint16",
-        'aantalfueltransacties': "uint16",
-        'depositoyn': "uint8",
-        'saldodeposito': "uint32",
-        'flexibelsparenyn': "uint8",
-        'saldoflexibelsparen': "uint32",
-        'kwartaalsparenyn': "uint8",
-        'saldokwartaalsparen': "uint32",
-        'gemaksbeleggenyn': "uint8",
-        'saldogemaksbeleggen': "uint32",
-        'participatieyn': "uint8",
-        'saldoparticipatie': "uint32",
-        'vermogensbeheeryn': "uint8",
-        'saldovermogensbeheer': "uint32",
-        'saldototaal': "int32",
-        'saldolangetermijnsparen': "uint32",
-        'aantaltegenrekeningenlaatsteq': "uint16"}
-
-    aggfuncDict = {**aggFuncDictP2, **aggFuncDictP3}
-    return aggfuncDict
-
 
 def getPivotColumns():
     columnsPivot = [
