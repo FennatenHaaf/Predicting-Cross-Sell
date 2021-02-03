01986b1220ce8721ca41bb1cfbd97191ac18e091
mauddw <mauddw99@gmail.com>
2021-02-03 14:32:05 +0100

    Update maximization.py

--- maximization.py
+++ maximization.py
@@ -31,13 +31,31 @@ def maximization_step(Y, Z, , alpha, beta, gamma_0_in, gamma_sr_0_in, gamma_sk_t
     return [gamma_0_out, gamma_sr_0_out, gamma_sk_t_out, beta_out]
 
 def optimization_function(x, alpha, beta, Y, Z, T):
-    
+    gamma_0_out, gamma_sr_0_out, gamma_sk_t_out, beta_out = maximization_step(Y, Z, , alpha, beta, gamma_0_in, gamma_sr_0_in, gamma_sk_t_in, beta_in, T)
+    initialstateprob = ones(N,S)
+    transitionmatrix = ones(N,S)
+    stateprob = ones(NS,S)
+    for i in range(1,N):
+        stateprob[i] = joint_event(beta_out)
+        [initialstateprob[i], transitionmatrix[i]] = state_event(Z, gamma_0_out, gamma_sr_0_out)
     return 
     
-def joint_event():
-    
-    return
+def joint_event(beta_out):
+    pi = ones(J, S)
+    for j in range(1,J):
+        for s in range(1,S):
+            pi[j,s] = 1/(1+exp(sum(beta_out[])))
+    return pi
 
-def state_event():
-    
-    return
+def state_event(Z, gamma_0_out, gamma_sr_0_out):
+    gamma_0_val = gamma_0_out*Z
+    S_0 = exp(gamma_val[S])
+    initial = ones((S,1))
+    for i in range(1, S-1):
+        initial[i] = exp(gamma_val[i])*S
+    gamma_val = gamma_sr_0_out*Z
+    prob = ones(())
+    for t in range(1,T):
+        s_t[t] = exp(gamma_val[t])
+        prob[] = exp(gamma_val[t])*s_t[t]
+    return prob, initial
