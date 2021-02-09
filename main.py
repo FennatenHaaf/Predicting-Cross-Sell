84de09a1930bf13b11c0ae7c1ba798709258205e
FennatenHaaf <61841371+FennatenHaaf@users.noreply.github.com>
2021-02-09 08:42:43 +0100

    finished business variables & made time series function

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ if __name__ == "__main__":
     indirec = "./data"
     outdirec = "./output"
     interdir = "./interdata"
-    save_intermediate_results = True # Save the intermediate outputs
+    save_intermediate_results = False # Save the intermediate outputs
     print_information = False # Print things like frequency tables or not
     quarters = True # In which period do we want to aggregate the data?
+    cross_sec = False
+    time_series = True
+    
     # TODO zou ook quarterly een .self variabele kunnnen maken in dataprocessor,
     # alleen dan kan het niet verschillen per dataset maar dat is misschien niet erg
     
@@ -24,26 +27,36 @@ if __name__ == "__main__":
     # initialise the base linked data and the base Experian data which are
     # used to create the datasets
     test.link_data() 
-    #Create base experian information and select the ids used
+    #Create base experian information and select the ids used -> choose to
+    # make subsample here! 
     test.select_ids(quarterly = quarters, subsample = True,
                    sample_size = 500, start_date = "2018", 
                    outname = "base_experian")
     
+    #TODO: make subsample variable at the top, also remove subsample
+    # from create_base_crosssection function
     
     #----------------MAKE CROSS-SECTIONAL DATASETS-------------------
+    if cross_sec:
+        df_cross, cross_date = test.create_base_cross_section(date_string="2020-12", 
+                            subsample=False, sample_size = 1000,next_period = False, 
+                            quarterly=quarters)
+        
+        
+        df_cross_link = test.create_cross_section_perportfolio(df_cross, cross_date, 
+                                              outname = "df_cross_portfoliolink",
+                                              quarterly= quarters)
+        
+        test.create_cross_section_perperson(df_cross, df_cross_link,
+                                            cross_date, outname = "final_df_quarterly",
+                                            quarterly= quarters)
     
-    df_cross, cross_date, df_next, next_date = test.create_base_cross_section(
-        date_string="2020-12", subsample=False, sample_size = 1000, quarterly=quarters)
-    
-    
-    df_cross_link = test.create_cross_section_perportfolio(df_cross, cross_date, 
-                                          outname = "df_cross_portfoliolink",
-                                          quarterly= quarters)
-    
-    test.create_cross_section_perperson(df_cross, df_cross_link,
-                                        cross_date, outname = "final_df_quarterly",
-                                        quarterly= quarters)
-    
+    #----------------MAKE TIME SERIES DATASETS-------------------
+   
+    if time_series:
+        test.time_series_from_cross(quarterly = quarters,
+                   sample_size = 500, start_date = "2018-01-01")
+        
     
     #-----------------------------------------------------------------
     
