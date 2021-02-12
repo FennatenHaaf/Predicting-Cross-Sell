0e3edea65278e9b4bbfcf0c37c59e9240db39f28
ivankalia <77011392+ivankalia@users.noreply.github.com>
2021-02-12 14:09:11 +0100

    wrong syntax to comment repair

--- test_set.py
+++ test_set.py
@@ -36,10 +36,10 @@ name_dep_var_active = ['ac1', 'ac2', 'ac3']
 #Bool: If true: run Model as paas , if False: run a more general HMM
 covariates = False
 
-/test = ht.HMM_eff(df_per_time, name_dep_var, name_covariates, covariates)
-/n_segments = 4
+#test = ht.HMM_eff(df_per_time, name_dep_var, name_covariates, covariates)
+#n_segments = 4
 
-/gamma_0, gamma_sr_0, gamma_sk_t, beta = test.EM(n_segments, max_method = 'Nelder-Mead')
+#gamma_0, gamma_sr_0, gamma_sk_t, beta = test.EM(n_segments, max_method = 'Nelder-Mead')
 
 n_segments = 3
 test_active = ht.HMM_eff(df_per_time, name_dep_var_active, False)
